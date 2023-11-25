ade7041d6c1951c660bba86dcb8e89e2cea6f34e
GalsanovTS <GalsanovTS@309D-6>
2023-11-25 13:43:33 +0700

    Добавьте файлы проекта.

---
+++ DB_Library/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace DB_Library
+{
+    public class DatabaseManager
+    {
+        private readonly string connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+    }
+
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+class Program
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("https://www.cyberforum.ru/csharp-beginners/thread2787061.html");
+
+        var comments = driver.FindElements(By.CssSelector("#post15280941 > tbody"));
+        Console.WriteLine(comments.Count);
+
+        foreach (var comment in comments)
+        {
+            var username = comment.FindElement(By.CssSelector("#post15280941 > tbody > tr:nth-child(1) > td > div:nth-child(1) > a")).Text; 
+            var commentText = comment.FindElement(By.CssSelector("#post_message_15280941")).Text;
+
+            Console.WriteLine($"Username: {username} - Comment: {commentText}");
+        }
+
+        driver.Quit();
+    }
+}
