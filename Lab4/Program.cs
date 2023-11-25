3714cc30792c570ac7c2ff6fab0d0170155c13f7
GalsanovTS <133204491+GalsanovTS@users.noreply.github.com>
2023-11-25 13:49:05 +0700

    Update Program.cs

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -9,15 +9,15 @@ class Program
     {
         IWebDriver driver = new ChromeDriver();
 
-        driver.Navigate().GoToUrl("https://www.cyberforum.ru/csharp-beginners/thread2787061.html");
+        driver.Navigate().GoToUrl("https://www.cyberforum.ru/asp-net-mvc/thread3138549.html");
 
-        var comments = driver.FindElements(By.CssSelector("#post15280941 > tbody"));
+        var comments = driver.FindElements(By.CssSelector("#post17103033 > tbody > tr:nth-child(1) > td > div:nth-child(1) > a > tbody"));
         Console.WriteLine(comments.Count);
 
         foreach (var comment in comments)
         {
-            var username = comment.FindElement(By.CssSelector("#post15280941 > tbody > tr:nth-child(1) > td > div:nth-child(1) > a")).Text; 
-            var commentText = comment.FindElement(By.CssSelector("#post_message_15280941")).Text;
+            var username = comment.FindElement(By.CssSelector("#post17103033 > tbody > tr:nth-child(1) > td > div:nth-child(1) > a")).Text; 
+            var commentText = comment.FindElement(By.CssSelector("#post_message_17103033")).Text;
 
             Console.WriteLine($"Username: {username} - Comment: {commentText}");
         }
